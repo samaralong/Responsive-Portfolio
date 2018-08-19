501e618e119245df3f7c11121a146091a74d5c46
Samara <long.samara@gmail.com>
2018-08-18 23:28:42 -0500

    Initial commit

---
+++ logo.js
@@ -0,0 +1,9 @@
+function onHover(element)
+{
+    element.setAttribute('src', 'https://i.imgur.com/4H7Cgkw.jpg');
+}
+
+function offHover(element)
+{
+    element.setAttribute('src', 'https://i.imgur.com/dZgIpr4.jpg');
+}
